7fdc584435cdcfaa4fa5c4f52b028d79966f2794
DESKTOP-OSC88PS\pakir <pakirathan.sophie@gmail.com>
2025-04-10 12:53:27 +0200

    Correction erreur

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
+    "node": true,
     "jest/globals": true
   },
   "extends": [
@@ -45,5 +46,10 @@ module.exports = {
     ],
     "no-console": "error",
     "react/prop-types": 0
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
   }
 }
